d0e3d6dcf846a23518e99e79be9e3fad96e1feb0
anidev <anidev.aelico@gmail.com>
2013-01-13 18:55:25 -0500

    Fixed bug

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main() {
 	double angle=10;
-	double v0=30.0;
+	double v0=50.0;
 	double vx0=v0*cos(torad(angle));
 	double vy0=v0*sin(torad(angle));
-	std::cout<<simulate_flight(vx0,vy0,5,0.1,24.384)<<std::endl;
+	std::cout<<simulate_flight(vx0,vy0,angle,0.1,24.384)<<std::endl;
 }
